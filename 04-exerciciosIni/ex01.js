51e201addf79aa9c4649159f9a030596905dad20
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-04-13 23:14:50 -0300

    exs

---
+++ 04-exerciciosIni/ex01.js
@@ -0,0 +1,53 @@
+//ex01
+
+
+/*function calMEdia (n1, n2, n3) {
+    med = (n1 + n2 + n3) / 3
+    return med;
+}
+
+
+console.log(calMEdia(5,6,7));
+
+
+//ex01Array
+const notas = [10,5,10];
+let soma = 0;
+function calcMedia () {
+    for (let i = 0; i < notas.length; i++) {
+        const nota = notas[i];
+        soma = soma + nota;      
+    }
+    media = soma/notas.length;
+        return media;
+}
+
+
+console.log(calcMedia());*/
+
+//ex02
+/*function calcTab(n) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${n} x ${i} = ${n*i}`);
+    }
+}    
+
+calcTab(9);*/
+
+
+
+//ex03
+
+/*const numeros = [1,2,3,4,5,6,7,8]; 
+function par() {
+    for (let i = 0; i < numeros.length; i++) {
+        const numero = numeros[i];
+        if (numero % 2 === 0){
+            console.log(numero);
+        }
+        
+    }
+
+}
+
+par();*/
