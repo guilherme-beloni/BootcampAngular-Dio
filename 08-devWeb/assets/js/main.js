166175c300fbea0028157e90534aed74c40b7283
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-04-16 16:24:04 -0300

    consumindoApi

--- 08-devWeb/assets/js/main.js
+++ 08-devWeb/assets/js/main.js
@@ -1,1 +1,33 @@
-console.log('Sucessfully');
+
+function convertPokemonToLiHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+                
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}">
+            </div>
+                
+        </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons()
+    .then( (pokemons) => {
+        for (let i = 0; i < pokemons.length; i++){
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLiHtml(pokemon)
+        }    
+    })
+
+    
+    .catch( (error) => console.log(error))
+
+
+    console.log('Sucessfully');
