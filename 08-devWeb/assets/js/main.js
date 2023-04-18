b7db628af2247cce1931752dc5be9ff7773bd49d
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-04-17 21:16:59 -0300

    pokemon.Model

--- 08-devWeb/assets/js/main.js
+++ 08-devWeb/assets/js/main.js
@@ -1,20 +1,14 @@
-function convertTypesToLiHtml(pokemonsTypes) {
-    return pokemonsTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
-
-
 function convertPokemonToLiHtml(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
                 
             <div class="detail">
                 <ol class="types">
-                    ${convertTypesToLiHtml(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                 </ol>
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
                 
         </li>`
